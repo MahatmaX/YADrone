7a7d501f52d989b1c0784b652cf65597af03ceaa
MahatmaX <github@hyfy.de>
2013-09-09 13:42:43 +0200

    added option to set dimension for QR code generation added set of shreds for paperchase demo

--- YADrone/src/de/yadrone/apps/controlcenter/plugins/qrcode/generator/QRCodeGeneratorPanel.java
+++ YADrone/src/de/yadrone/apps/controlcenter/plugins/qrcode/generator/QRCodeGeneratorPanel.java
@@ -6,7 +6,6 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.Image;
 import java.awt.Insets;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
@@ -22,6 +21,7 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
@@ -36,9 +36,24 @@ public class QRCodeGeneratorPanel extends JPanel implements ICCPlugin
 {
 	private JLabel image = new JLabel();
 	private JTextArea text;
+	private JTextField width;
+	private JTextField height;
 	
 	public QRCodeGeneratorPanel()
 	{
+		Dimension dim = new Dimension(30,20);
+		width = new JTextField("300");
+		width.setSize(dim);
+		width.setPreferredSize(dim);
+		width.setMinimumSize(dim);
+		width.setMaximumSize(dim);
+		
+		height = new JTextField("300");
+		height.setSize(dim);
+		height.setPreferredSize(dim);
+		height.setMinimumSize(dim);
+		height.setMaximumSize(dim);
+		
 		text = new JTextArea("Enter your contents here");
 		generateQRCode(text.getText());
 		
@@ -60,10 +75,16 @@ public class QRCodeGeneratorPanel extends JPanel implements ICCPlugin
 		});
 		
 		setLayout(new GridBagLayout());
-		add(image, new GridBagConstraints(0, 0, 2, 1, 1, 0.7, GridBagConstraints.FIRST_LINE_START, GridBagConstraints.BOTH, new Insets(0,0,0,0), 0, 0));
-		add(text, new GridBagConstraints(0, 1, 2, 1, 1, 0.3, GridBagConstraints.FIRST_LINE_START, GridBagConstraints.BOTH, new Insets(0,0,0,0), 0, 0));
-		add(generateButton, new GridBagConstraints(0, 2, 1, 1, 0.5, 0.3, GridBagConstraints.FIRST_LINE_START, GridBagConstraints.HORIZONTAL, new Insets(0,0,0,0), 0, 0));
-		add(printButton, new GridBagConstraints(1, 2, 1, 1, 0.5, 0.3, GridBagConstraints.FIRST_LINE_START, GridBagConstraints.HORIZONTAL, new Insets(0,0,0,0), 0, 0));
+		add(image, new GridBagConstraints(0, 0, 6, 1, 1, 0.7, GridBagConstraints.FIRST_LINE_START, GridBagConstraints.BOTH, new Insets(0,0,0,0), 0, 0));
+		add(text, new GridBagConstraints(0, 1, 6, 1, 1, 0.3, GridBagConstraints.FIRST_LINE_START, GridBagConstraints.BOTH, new Insets(0,0,0,0), 0, 0));
+		
+		add(new JLabel("Width"),  new GridBagConstraints(0, 2, 1, 1, 0.1, 0.3, GridBagConstraints.FIRST_LINE_START, GridBagConstraints.HORIZONTAL, new Insets(4,0,0,0), 0, 0));
+		add(width,                  new GridBagConstraints(1, 2, 1, 1, 0.2, 0.3, GridBagConstraints.FIRST_LINE_START, GridBagConstraints.HORIZONTAL, new Insets(0,0,0,0), 0, 0));
+		add(new JLabel("Height"), new GridBagConstraints(2, 2, 1, 1, 0.1, 0.3, GridBagConstraints.FIRST_LINE_START, GridBagConstraints.HORIZONTAL, new Insets(4,4,0,0), 0, 0));
+		add(height,                 new GridBagConstraints(3, 2, 1, 1, 0.2, 0.3, GridBagConstraints.FIRST_LINE_START, GridBagConstraints.HORIZONTAL, new Insets(0,0,0,0), 0, 0));
+		
+		add(generateButton,         new GridBagConstraints(4, 2, 1, 1, 0.2, 0.3, GridBagConstraints.FIRST_LINE_START, GridBagConstraints.HORIZONTAL, new Insets(0,0,0,0), 0, 0));
+		add(printButton,            new GridBagConstraints(5, 2, 1, 1, 0.2, 0.3, GridBagConstraints.FIRST_LINE_START, GridBagConstraints.HORIZONTAL, new Insets(0,0,0,0), 0, 0));
 	}
 	
 	private void generateQRCode(String contents)
@@ -72,7 +93,7 @@ public class QRCodeGeneratorPanel extends JPanel implements ICCPlugin
 		BitMatrix bitMatrix = null;
 		try
 		{
-			bitMatrix = writer.encode(contents, BarcodeFormat.QR_CODE, 300, 300);
+			bitMatrix = writer.encode(contents, BarcodeFormat.QR_CODE, Integer.parseInt(width.getText()), Integer.parseInt(height.getText()));
 			BufferedImage image = MatrixToImageWriter.toBufferedImage(bitMatrix);
 			
 			setImage(image);
@@ -82,6 +103,10 @@ public class QRCodeGeneratorPanel extends JPanel implements ICCPlugin
 			e.printStackTrace();
 		}
 		
+//		setSize(new Dimension(Math.min(300, Integer.parseInt(width.getText())), Math.min(400, Integer.parseInt(height.getText()))));
+//		setPreferredSize(new Dimension(Math.min(300, Integer.parseInt(width.getText())), Math.min(400, Integer.parseInt(height.getText()))));
+//		setMinimumSize(new Dimension(Math.min(300, Integer.parseInt(width.getText())), Math.min(400, Integer.parseInt(height.getText()))));
+//		setMaximumSize(new Dimension(Math.min(300, Integer.parseInt(width.getText())), Math.min(400, Integer.parseInt(height.getText()))));
 	}
 	
 	private void print()
@@ -106,7 +131,8 @@ public class QRCodeGeneratorPanel extends JPanel implements ICCPlugin
 
 				// Now we perform our rendering
 				g.drawImage(((ImageIcon) image.getIcon()).getImage(), 0, 0, image.getWidth(), image.getHeight(), null);
-
+				g.drawString(text.getText(), 20, image.getHeight());
+				
 				// tell the caller that this page is part
 				// of the printed document
 				return PAGE_EXISTS;
